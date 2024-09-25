b5375a1bad37d27ac2235660e4d65e4cfbbe2473
kfmx <tweem@hotmail.co.uk>
2024-09-25 14:02:57 +0200

    Environment setup - Symfony, Webpack Encore, Vue.js

---
+++ assets/vue/App.vue
@@ -0,0 +1,16 @@
+<!-- assets/vue/App.vue -->
+<template>
+    <div id="app">
+      <h1>{{ message }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        message: 'Testing Vue.js and Symfony!'
+      };
+    }
+  }
+  </script>
